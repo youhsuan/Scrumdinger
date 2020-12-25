87dbdfc0f68f03edff7eae8093058bdfe8b6c798
youhsuan <hsuannyu@gmail.com>
2020-12-26 00:21:05 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by YOU-HSUAN YU on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
